3ee6c29e64952cbf94bfe2dcde646bfcafd4be74
vitrokb <victor.brns2@gmail.com>
2024-05-21 12:22:16 -0300

    the birthday bar complete

--- the-birthday-bar/the-birthday-bar.js
+++ the-birthday-bar/the-birthday-bar.js
@@ -1,8 +1,22 @@
 function birthday(s, d, m) {
   let possibleCases = [];
+  let answer = 0;
 
-  for (let i of s) {
+  for (let i = 0; i < s.length; i++) {
+    let partOfArray = s.slice(i, i + m);
+    if (partOfArray.length == m) {
+      possibleCases.push(partOfArray);
+    }
   }
+
+  possibleCases.forEach((element) => {
+    const sum = element.reduce((acc, currentValue) => acc + currentValue, 0);
+    if (sum == d) {
+      answer += 1;
+    }
+  });
+
+  return answer;
 }
 
-birthday([1, 2, 1, 3, 2], 3, 2);
+birthday([1, 1, 1, 1, 1], 3, 2);
