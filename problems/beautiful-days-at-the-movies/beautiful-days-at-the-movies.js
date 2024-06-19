5a20ebdaa25dc1f99174d7c140e0d1afce1fa945
vitrokb <victor.brns2@gmail.com>
2024-06-19 15:21:22 -0300

    beautiful days at the movies

---
+++ problems/beautiful-days-at-the-movies/beautiful-days-at-the-movies.js
@@ -0,0 +1,24 @@
+function beautifulDays(i, j, k) {
+  function reverseNum(num) {
+    return parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num);
+  }
+
+  let amountOfBeautifulDays = 0;
+  const days = [];
+
+  for (let a = i; a <= j; a++) {
+    days.push(a);
+  }
+
+  days.forEach((day) => {
+    const difference = Math.abs(day - reverseNum(day));
+    const division = difference % k;
+    if (division == 0) {
+      amountOfBeautifulDays += 1;
+    }
+  });
+
+  console.log(amountOfBeautifulDays);
+}
+
+beautifulDays(20, 23, 6);
